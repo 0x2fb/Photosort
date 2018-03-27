06f18532cc560b8332f1b9fd1f09d5a31339a6b4
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-03-28 00:04:19 +0200

    Added get_date_taken with exifread

--- photosort.py
+++ photosort.py
@@ -1,5 +1,19 @@
 import os
+import exifread
 
-for (path, dirs, files) in os.walk(os.getcwd()):
-    for f in files:
-        print(f"{path}\\{f}")
+
+def get_image():
+    for (path, _, files) in os.walk(os.getcwd()):
+        for f in files:
+            yield f"{path}\\{f}"
+
+
+def get_date_taken(image):
+    with open(image, 'rb') as openimage:
+        exiftags = exifread.process_file(openimage, details=False)
+    return exiftags["EXIF DateTimeDigitized"]
+
+
+images = get_image()
+for i in range(3):
+    print(get_date_taken(next(images)))
