74d1a490fda5d0bca2f6c768d8aaa70cb2e21994
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-03-28 11:27:48 +0200

    Added functions for image size, file size and filename

--- photosort.py
+++ photosort.py
@@ -1,19 +1,43 @@
 import os
 import exifread
+from PIL import Image
 
 
 def get_image():
+    '''Generator object that yields all images [todo]
+    in a folder and its subfolders'''
     for (path, _, files) in os.walk(os.getcwd()):
         for f in files:
             yield f"{path}\\{f}"
 
 
-def get_date_taken(image):
-    with open(image, 'rb') as openimage:
-        exiftags = exifread.process_file(openimage, details=False)
-    return exiftags["EXIF DateTimeDigitized"]
+def get_date_taken(imagefile):
+    '''Returns the date the image was taken from its EXIF data'''
+    with open(imagefile, 'rb') as image:
+        exiftags = exifread.process_file(image, details=False)
+    return exiftags["EXIF DateTimeOriginal"]
+
+
+def get_image_size(imagefile):
+    '''Returns a tuple (w, h) that holds the width and height of an image'''
+    image = Image.open(imagefile)
+    return image.size
+
+
+def get_file_size(imagefile):
+    '''Returns size of the image in Bytes'''
+    return os.path.getsize(imagefile)
+
+
+def get_filename(imagefile):
+    '''Returns the filename of the image'''
+    return os.path.basename(imagefile)
 
 
 images = get_image()
 for i in range(3):
-    print(get_date_taken(next(images)))
+    image = next(images)
+    print(get_filename(image))
+    print(get_date_taken(image))
+    print(get_image_size(image))
+    print(get_file_size(image))
